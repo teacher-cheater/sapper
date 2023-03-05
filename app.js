5507844ad6d069cd8918445fd967fea39b3436be
teacher_cheater <johnnykayfun@gmail.com>
2023-03-05 12:46:25 +0300

    add field markup

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ const minesLocation = []; // 2-8
 
 const gameOver = false;
 
+window.onload = () => startGame();
+
+
 function startGame() {
   document.querySelector('#count').innerHTML = minesCount;
+
+  for (let i = 0; i < rows; i++) {
+    let row = []
+    for (let k = 0; k < columns; k++) {
+      let tile = document.createElement("div")
+      tile.id = `${i.toString()}-${k.toString()}`;
+      document.querySelector("#board").append(tile)
+      row.push(tile)
+    }
+    board.push(row)
+  }
+  console.log(board);
 }
 
-startGame()
+
+
